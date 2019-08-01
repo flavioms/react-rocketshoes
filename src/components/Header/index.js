f7957b4ac5527787014788d20928a84000cc188a
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-01 17:09:07 -0300

    consulta estoque ao adicionar no carrinho

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+import logo from '../../assets/images/logo.svg';
+
+function Header({ cartSize }) {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes Logo" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={30} color="FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
---
+++ src/store/modules/cart/sagas.js
@@ -0,0 +1,32 @@
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import api from '../../../services/api';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
+
+function* addToCard({ id }) {
+  const productExist = yield select(state => state.cart.find(p => p.id === id));
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExist ? productExist.amount : 0;
+  const amount = currentAmount + 1;
+
+  if (amount >= stockAmount) {
+    console.tron.warn('ERROR');
+    return;
+  }
+
+  if (productExist) {
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
+}
+
+export default all([takeLatest('@cart/ADD_REQUEST', addToCard)]);
